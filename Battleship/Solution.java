ad8cee43bc49ef6497fea019223d32014289fd36
SreeramCharagundla <122760187+SreeramCharagundla@users.noreply.github.com>
2024-06-22 00:41:10 -0400

    solved Battleship

---
+++ Battleship/Solution.java
@@ -0,0 +1,17 @@
+package Battleship;
+
+class Solution {
+    public static void main(String[] args) {
+        int R = 0;
+        int C = 0;
+        int[][] G = new int[][]{{0,0,0},{0,0,0},{0,0,0}};
+        double ans = getHitProbability(R, C, G);
+
+        System.out.println(ans);
+    }
+
+    public static double getHitProbability(int R, int C, int[][] G) {
+        // Write your code here
+        return 0.0;
+      }
+}
